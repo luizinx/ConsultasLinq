f2d3bc1daaa74457a00616dd1f2a67b7a8afb7df
Luiz Fernando <conttatoluiz@gmail.com>
2023-05-23 23:19:32 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultasQuerys/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsultasLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            IEnumerable<int> result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            foreach (int x in result) {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
